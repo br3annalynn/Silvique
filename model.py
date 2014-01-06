bba37fdfa21aecd61f32413826818694018e013c
Breanna <breannalynn@gmail.com>
2014-01-06 13:38:12 -0500

    Compare inventory is working

--- model.py
+++ model.py
@@ -8,9 +8,12 @@ def connect_to_db():
     CONN = sqlite3.connect('inventory.db')
     DB = CONN.cursor()
 
-def check_inventory(bar_code, value):
+def check_inventory(file_type, bar_code, value):
     connect_to_db()
-    query = """SELECT amount, singleValue, totalValue FROM inventory WHERE barCode = ?"""
+    if file_type == "I":
+        query = """SELECT amount, singleValue, totalValue FROM inventory WHERE barCode = ?"""
+    else:
+        query = """SELECT amount, singleValue, totalValue FROM comparison WHERE barCode = ?"""
     DB.execute(query, (bar_code,))
     row = DB.fetchone()
     if not row:
@@ -18,34 +21,85 @@ def check_inventory(bar_code, value):
         single_value = value
         total_value = value
     else:
-        delete_row(bar_code)
+        delete_row(file_type, bar_code)
         amount = row[0] + 1
         single_value = row[1]
         total_value = single_value * amount
-    add_to_inventory(bar_code, amount, single_value, total_value)
+    add_to_inventory(file_type, bar_code, amount, single_value, total_value)
 
-def delete_row(bar_code):
+def delete_row(file_type, bar_code):
     connect_to_db()
-    query = """DELETE FROM inventory WHERE barCode = ?"""
+    if file_type == "I":
+        query = """DELETE FROM inventory WHERE barCode = ?"""
+    else:
+        query = """DELETE FROM comparison WHERE barCode = ?"""
     DB.execute(query, (bar_code,))
     CONN.commit()
     print "deleted item", bar_code
 
-def add_to_inventory(bar_code, amount, single_value, total_value):
+def add_to_inventory(file_type, bar_code, amount, single_value, total_value):
     connect_to_db()
-    query = """INSERT into inventory values (?, ?, ?, ?)"""
+    if file_type == "I":
+        query = """INSERT into inventory values (?, ?, ?, ?)"""
+    else:
+        query = """INSERT into comparison values (?, ?, ?, ?)"""
     DB.execute(query, (bar_code, amount, single_value, total_value))
     CONN.commit()
     print "Added item", bar_code
 
 def show_inventory():
     connect_to_db()
-    query = """SELECT * from inventory ORDER BY barCode"""
+    query = """SELECT * from inventory ORDER BY barCode COLLATE NOCASE"""
     DB.execute(query, ())
     rows = DB.fetchall()
     return rows
 
+def clear(file_type):
+    connect_to_db()
+    if file_type == "I":
+        query = """DELETE from inventory"""
+    else:
+        query = """DELETE from comparison"""
+    DB.execute(query, ())
+    CONN.commit()
+    print "All rows deleted"
 
+
+def show_comparison():
+    connect_to_db()
+    # check if there is a comparison file uploaded
+    query = """SELECT * from comparison"""
+    DB.execute(query, ())
+    rows = DB.fetchall()
+
+    # find items only in inventory
+    query = """SELECT barCode from inventory where barCode not in (select barCode from comparison)"""
+    DB.execute(query, ())
+    inventory_only_bar_codes = DB.fetchall()
+    inventory_only_items = []
+    for code in inventory_only_bar_codes:
+        query = """SELECT * from inventory where barCode = ?"""
+        DB.execute(query, (code[0],))
+        item = DB.fetchone()
+        inventory_only_items.append(item)
+
+    #find items only in comparison
+    query = """SELECT barCode from comparison where barCode not in (select barCode from inventory)"""
+    DB.execute(query, ())
+    comparison_only_bar_codes = DB.fetchall()
+    comparison_only_items = []
+    for code in comparison_only_bar_codes:
+        query = """SELECT * from comparison where barCode = ?"""
+        DB.execute(query, (code[0],))
+        item = DB.fetchone()
+        comparison_only_items.append(item)
+
+    # find items in both with unequal amounts
+    query = """SELECT * from inventory join comparison on inventory.barCode = comparison.barCode where inventory.amount != comparison.amount"""
+    DB.execute(query, ())
+    unequal_items = DB.fetchall()
+
+    return (rows, inventory_only_items, comparison_only_items, unequal_items)
 
 
 
--- silvique.py
+++ silvique.py
@@ -6,6 +6,8 @@ import datetime
 app = Flask(__name__)
 app.secret_key = "shhhhthisisasecret"
 
+XLS_FOLDER = 'xls/'
+
 @app.route("/")
 def index():
     return render_template('main.html')
@@ -18,23 +20,23 @@ def display_inventory():
         total += row[3]
     return render_template('show_inventory.html', inventory_list=inventory_list, total=total)
 
+
 @app.route("/upload_inv")
 def upload_inv():
     return render_template('upload.html')
-    ###########change file name to file with inventory
-    # book = open_workbook('simple.xls')
-    # sheet = book.sheet_by_index(0)
-    # read_bar_codes(sheet)
-    # return redirect(url_for('display_inventory'))
+
 
 @app.route("/upload_inv", methods=["POST"])
 def upload_inventory():
+    global XLS_FOLDER
     file_name = request.form.get('file')
     print "******************Trying to open ", file_name
     try:
-        book = open_workbook(file_name)
+        book = open_workbook(XLS_FOLDER + file_name)
         sheet = book.sheet_by_index(0)
-        read_bar_codes(sheet)
+        # I for inventory, C for comparison
+        file_type = 'I'
+        read_bar_codes(file_type, sheet)
         return redirect(url_for('display_inventory'))
     except IOError:
         flash("File not found. Check file name. ")
@@ -42,18 +44,43 @@ def upload_inventory():
         return redirect(url_for('upload_inventory'))
     
 
-def read_bar_codes(sheet):
-    # for row_index in range(sheet.nrows):
-    for row_index in range(30, 40):
+def read_bar_codes(file_type, sheet):
+    for row_index in range(sheet.nrows):
         bar_code = sheet.cell(row_index,0).value
         # change last three elements of string to integer
         value = convert_to_int(sheet.cell(row_index,0).value[-3], sheet.cell(row_index,0).value[-2], sheet.cell(row_index,0).value[-1])
-        model.check_inventory(bar_code, value)
+        model.check_inventory(file_type, bar_code, value)
 
 def convert_to_int(a, b, c):
     number = 0
     number += (ord(a) - 48) * 100 + (ord(b) - 48) * 10 + (ord(c) - 48)
     return number
+
+@app.route("/upload_compare")
+def upload_compare():
+    return render_template('upload_compare.html')
+
+@app.route("/upload_compare", methods=["POST"])
+def upload_comparison():
+    global XLS_FOLDER
+    file_name = request.form.get('file')
+    print "******************Trying to open ", file_name
+    try:
+        book = open_workbook(XLS_FOLDER + file_name)
+        sheet = book.sheet_by_index(0)
+        # I for inventory, C for comparison
+        file_type = 'C'
+        read_bar_codes(file_type, sheet)
+        return redirect(url_for('display_comparison'))
+    except IOError:
+        flash("File not found. Check file name. ")
+        print "No file found with the name ", file_name
+        return redirect(url_for('upload_compare'))
+
+@app.route("/display_compare")
+def display_comparison():
+    (rows, inventory_only_items, comparison_only_items, unequal_items) = model.show_comparison()
+    return render_template('display_compare.html', rows=rows, inventory_only_items=inventory_only_items, comparison_only_items=comparison_only_items, unequal_items=unequal_items)
 
 @app.route("/print_view")
 def print_view():
@@ -64,6 +91,15 @@ def print_view():
         total += row[3]
     return render_template('print_view.html', inventory_list=inventory_list, total=total, current_date=current_date)
 
-    
+@app.route("/delete_inv")
+def delete_inventory():
+    model.clear("I")
+    return redirect(url_for('display_inventory'))
+
+@app.route("/delete_compare")
+def delete_comparison():
+    model.clear('C')
+    return redirect(url_for('display_comparison'))
+
 if __name__ == "__main__":
     app.run(debug=True)
