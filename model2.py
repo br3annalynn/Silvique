7e58953c12bae58a6438ca16c66bbad4d0cd3941
Breanna <breannalynn@gmail.com>
2014-01-27 13:29:02 -0500

    Inventory view and print view working

--- model2.py
+++ model2.py
@@ -1,10 +1,13 @@
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy import create_engine
-from sqlalchemy import Column, Integer, String, Date
+from sqlalchemy import Column, Integer, String, Date, ForeignKey, func
+from sqlalchemy.orm import sessionmaker, relationship, backref, scoped_session
+
+engine = create_engine("sqlite:///inventory.db", echo=False)
+session = scoped_session(sessionmaker(bind=engine, autocommit = False, autoflush = False))
 
 Base = declarative_base()
-
-
+Base.query = session.query_property()
 
 ### Class declarations go here
 class User(Base):
@@ -28,7 +31,7 @@ class Packing_list(Base):
     date = Column(Date, nullable=False)
     name = Column(String(64), nullable=False)
 
-class item(Base):
+class Item(Base):
     __tablename__ = "items"
 
     id = Column(Integer, primary_key = True, nullable=False)
@@ -36,13 +39,42 @@ class item(Base):
     amount = Column(Integer, nullable=False)
     tag = Column(Integer, nullable=False)
     total = Column(Integer, nullable=False)
-    sale_id = Column(Integer, nullable=False)
-    packing_list_id = Column(Integer, nullable=False)
+    sale_id = Column(Integer, ForeignKey('sales.id'), nullable=False)
+    packing_list_id = Column(Integer, ForeignKey('packing_lists.id'), nullable=False)
 
+    sale = relationship("Sale", backref=backref("items", order_by=id))
+    packing_list = relationship("Packing_list", backref=backref("items", order_by=id))
 ### End class declarations
 
+def login(name, password):
+    user = session.query(User).filter_by(name=name).one()
+    if user.password == password:
+        return user.id
+
+def add_packing_list(name, date):
+    packing_list =Packing_list(date=date, name=name)
+    session.add(packing_list)
+    session.commit()
+    return packing_list.id
+
+def add_sale(location, date):
+    sale = Sale(date=date, location=location)
+    session.add(sale)
+    session.commit()
+    return sale.id
+
+def add_item(pl_id, sale_id, bar_code, value, amount):
+    item = Item(sku=bar_code, amount=amount, tag=value, total=value * amount, sale_id=sale_id, packing_list_id=pl_id)
+    session.add(item)
+    session.commit()
+
+def show_inventory():
+    rows = session.query(Item.sku, func.sum(Item.amount), Item.tag, Item.total).group_by(Item.sku).all()
+    print "###########rows: ", rows
+    return rows
+
+
 def main():
-    """In case we need this for something"""
     pass
 
 if __name__ == "__main__":
--- silvique.py
+++ silvique.py
@@ -1,5 +1,6 @@
 from xlrd import open_workbook, cellname
-import model
+# import model
+import model2
 from flask import Flask, render_template, request, redirect, session, url_for, flash
 import datetime
 from xlwt import Workbook
@@ -14,46 +15,69 @@ XLS_FOLDER = 'xls/'
 def index():
     return render_template('main.html')
 
+@app.route("/", methods=['POST'])
+def login():
+    submitted_name = request.form.get('name')
+    submitted_password = request.form.get('password')
+
+    user_id = model2.login(submitted_name, submitted_password)
+    if user_id:
+        session['session_user_id'] = user_id 
+        print "Successfully loged in"
+        return redirect(url_for("display_inventory"))
+    else:
+        flash("Username or password incorect.")
+        return redirect(url_for("process_login"))
+
 @app.route("/show")
 def display_inventory():
-    inventory_list = model.show_inventory()
+    inventory_list = model2.show_inventory()
     total = 0
     for row in inventory_list:
         total += row[3]
     return render_template('show_inventory.html', inventory_list=inventory_list, total=total)
 
-@app.route("/get_lists")
-def get_lists():
-    inventory_list = []
-    sales_list = []
-    rows = model.list_tables()
-    for row in rows:
-        first_letter = row[0][0]
-        if first_letter == "i" or first_letter == "I":
-            # skip the main inventory table
-            if row[0] != "inventory":
-                inventory_list.append(row[0])
-        elif first_letter == "s" or first_letter == "S":
-            sales_list.append(row[0])
-    return json.dumps({'inventory_list' : inventory_list, 'sales_list': sales_list})
+# @app.route("/get_lists")
+# def get_lists():
+#     inventory_list = []
+#     sales_list = []
+#     rows = model.list_tables()
+#     for row in rows:
+#         first_letter = row[0][0]
+#         if first_letter == "i" or first_letter == "I":
+#             # skip the main inventory table
+#             if row[0] != "inventory":
+#                 inventory_list.append(row[0])
+#         elif first_letter == "s" or first_letter == "S":
+#             sales_list.append(row[0])
+#     return json.dumps({'inventory_list' : inventory_list, 'sales_list': sales_list})
 
 
 @app.route("/upload_inv")
 def upload_inv():
-    return render_template('upload.html')
-
+    session_user_id = session.get('session_user_id')
+    if session_user_id:
+        return render_template('upload.html')
+    return redirect(url_for('index'))
 
 @app.route("/upload_inv", methods=["POST"])
 def upload_inventory():
     global XLS_FOLDER
     file_name = request.form.get('file')
+    name = request.form.get('name')
+    try:
+        date = datetime.datetime.strptime(request.form.get('date'), '%m/%d/%Y')
+    except ValueError:
+        flash("Enter four digit year")
+        return redirect(url_for('upload_inventory'))
     print "******************Trying to open ", file_name
     try:
         book = open_workbook(XLS_FOLDER + file_name)
-        sheet = book.sheet_by_index(1)
-        # I for inventory, C for comparison, S for sale
-        file_type = 'I'
-        read_bar_codes(file_name, file_type, sheet)
+        sheet = book.sheet_by_index(0)
+        sale_id = 0
+        pl_id = model2.add_packing_list(name, date)
+        print "This is the pl_id: ", pl_id
+        read_bar_codes(pl_id, sale_id, sheet)
         return redirect(url_for('display_inventory'))
     except IOError:
         flash("File not found. Check file name. ")
@@ -61,45 +85,43 @@ def upload_inventory():
         return redirect(url_for('upload_inventory'))
     
 
-def read_bar_codes(file_name, file_type, sheet):
-    if file_type == "S":
+def read_bar_codes(pl_id, sale_id, sheet):
+    if sale_id:
         start = 6
-    elif file_type == "I":
-        start = 5
     else:
-        start = 3
+        start = 5
     for row_index in range(start, sheet.nrows):
         bar_code = sheet.cell(row_index,0).value
-        amount = find_amount(file_type, sheet, row_index)
+        amount = find_amount(pl_id, sale_id, sheet, row_index)
         # check that cell is not empty
         if bar_code:
             bar_code = bar_code.upper()
-            # change last three elements of string to integer
+            # check that sku is valid (ends in 3 numbers)
             try:
+                # change last three elements of string to integer
                 value = convert_to_int(sheet.cell(row_index,0).value[-3], sheet.cell(row_index,0).value[-2], sheet.cell(row_index,0).value[-1])
             except TypeError:
                 flash(bar_code)
                 print "Not a valid sku. Skipping ", bar_code
                 continue
-            # check that sku is valid (ends in 3 numbers)
             if not value:
                 flash(bar_code)
                 print "Not a valid sku. Skipping ", bar_code
                 continue
-            model.check_inventory(file_type, bar_code, value, amount)
-            model.add_to_table(file_type, file_name, bar_code, value, amount)
+            model2.add_item(pl_id, sale_id, bar_code, value, amount)
+            # model.check_inventory(file_type, bar_code, value, amount)
         else:
             continue
 
-def find_amount(file_type, sheet, row_index):
-    if file_type == "S":
+def find_amount(pl_id, sale_id, sheet, row_index):
+    if sale_id:
         amount = -1
     else:
         ################ check where this amount is. Add to template.
-        amount = sheet.cell(row_index, 1).value
+        amount = sheet.cell(row_index, 2).value
     # set default value of 1
-    # if not amount:
-    #     amount = 1
+    if not amount:
+        amount = 1
     return amount
 
 
@@ -108,57 +130,56 @@ def convert_to_int(a, b, c):
     if ord(a) < 48 or 57 < ord(a) or ord(b) < 48 or 57 < ord(b) or ord(c) < 48 or 57 < ord(c):
         print ord(a), ord(b), ord(c)
         return False
-    number = 0
-    number += (ord(a) - 48) * 100 + (ord(b) - 48) * 10 + (ord(c) - 48)
+    number = (ord(a) - 48) * 100 + (ord(b) - 48) * 10 + (ord(c) - 48)
     return number
 
 @app.route("/print_view")
 def print_view():
     current_date = datetime.date.today().strftime("%m/%d/%y")
-    inventory_list = model.show_inventory()
+    inventory_list = model2.show_inventory()
     total = 0
     for row in inventory_list:
         total += row[3]
     return render_template('print_view.html', inventory_list=inventory_list, total=total, current_date=current_date)
 
-@app.route("/delete_inv")
-def delete_inventory():
-    model.clear("I")
-    return redirect(url_for('display_inventory'))
+# @app.route("/delete_inv")
+# def delete_inventory():
+#     model.clear("I")
+#     return redirect(url_for('display_inventory'))
 
 
-@app.route("/display_compare", methods=["POST"])
-def upload_comparison():
-    global XLS_FOLDER
-    file_name = request.form.get('file')
-    print "******************Trying to open ", file_name
-    try:
-        book = open_workbook(XLS_FOLDER + file_name)
-        sheet = book.sheet_by_index(0)
-        # I for inventory, C for comparison, S for sale
-        file_type = 'C'
-        read_bar_codes(file_name, file_type, sheet)
-        return redirect(url_for('display_comparison'))
-    except IOError:
-        flash("File not found. Check file name. ")
-        print "No file found with the name ", file_name
-        return redirect(url_for('upload_comparison'))
+# @app.route("/display_compare", methods=["POST"])
+# def upload_comparison():
+#     global XLS_FOLDER
+#     file_name = request.form.get('file')
+#     print "******************Trying to open ", file_name
+#     try:
+#         book = open_workbook(XLS_FOLDER + file_name)
+#         sheet = book.sheet_by_index(0)
+#         # I for inventory, C for comparison, S for sale
+#         file_type = 'C'
+#         read_bar_codes(file_name, file_type, sheet)
+#         return redirect(url_for('display_comparison'))
+#     except IOError:
+#         flash("File not found. Check file name. ")
+#         print "No file found with the name ", file_name
+#         return redirect(url_for('upload_comparison'))
 
-@app.route("/display_compare")
-def display_comparison():
-    (rows, inventory_only_items, comparison_only_items, unequal_items) = model.show_comparison()
-    return render_template('display_compare.html', rows=rows, inventory_only_items=inventory_only_items, comparison_only_items=comparison_only_items, unequal_items=unequal_items)
+# @app.route("/display_compare")
+# def display_comparison():
+#     (rows, inventory_only_items, comparison_only_items, unequal_items) = model.show_comparison()
+#     return render_template('display_compare.html', rows=rows, inventory_only_items=inventory_only_items, comparison_only_items=comparison_only_items, unequal_items=unequal_items)
 
-@app.route("/print_view_compare")
-def print_view_compare():
-    current_date = datetime.date.today().strftime("%m/%d/%y")
-    (rows, inventory_only_items, comparison_only_items, unequal_items) = model.show_comparison()
-    return render_template('print_view_compare.html', rows=rows, inventory_only_items=inventory_only_items, comparison_only_items=comparison_only_items, unequal_items=unequal_items, current_date=current_date)
+# @app.route("/print_view_compare")
+# def print_view_compare():
+#     current_date = datetime.date.today().strftime("%m/%d/%y")
+#     (rows, inventory_only_items, comparison_only_items, unequal_items) = model.show_comparison()
+#     return render_template('print_view_compare.html', rows=rows, inventory_only_items=inventory_only_items, comparison_only_items=comparison_only_items, unequal_items=unequal_items, current_date=current_date)
 
-@app.route("/delete_compare")
-def delete_comparison():
-    model.clear('C')
-    return redirect(url_for('display_comparison'))
+# @app.route("/delete_compare")
+# def delete_comparison():
+#     model.clear('C')
+#     return redirect(url_for('display_comparison'))
 
 
 @app.route("/upload_sale")
@@ -182,64 +203,71 @@ def upload_sales_report():
         print "No file found with the name ", file_name
         return redirect(url_for('upload_sale'))
 
-@app.route("/save_inventory")
-def save_inv():
-    return render_template('save_inventory.html')
+# @app.route("/save_inventory")
+# def save_inv():
+#     return render_template('save_inventory.html')
 
-@app.route("/save_inventory", methods=['POST'])
-def save_inventory():
-    current_date = datetime.date.today().strftime("%m/%d/%y")
-    global XLS_FOLDER
-    book = Workbook()
-    sheet1 = book.add_sheet('inventory')
-    file_name = request.form.get('file')
-    inventory_list = model.show_inventory()
-    sheet1.write(0, 0, "Inventory " + current_date) 
-    sheet1.write(1, 0, "Tag Total")
-    sheet1.write(2, 0, "1/2 Tag Total")
-    sheet1.write(4, 0, "Sku")
-    sheet1.write(4, 1, "#")
-    sheet1.write(4, 2, "Tag")
-    sheet1.write(4, 3, "Total")
+# @app.route("/save_inventory", methods=['POST'])
+# def save_inventory():
+#     current_date = datetime.date.today().strftime("%m/%d/%y")
+#     global XLS_FOLDER
+#     book = Workbook()
+#     sheet1 = book.add_sheet('inventory')
+#     file_name = request.form.get('file')
+#     inventory_list = model.show_inventory()
+#     sheet1.write(0, 0, "Inventory " + current_date) 
+#     sheet1.write(1, 0, "Tag Total")
+#     sheet1.write(2, 0, "1/2 Tag Total")
+#     sheet1.write(4, 0, "Sku")
+#     sheet1.write(4, 1, "#")
+#     sheet1.write(4, 2, "Tag")
+#     sheet1.write(4, 3, "Total")
 
-    total = 0
-    single_row = 0
-    while single_row < len(inventory_list):
-        print_row = sheet1.row(single_row + 5)
-        for i in range(0, 4):
-            print_row.write(i, inventory_list[single_row][i])
-        total += inventory_list[single_row][3]
-        single_row +=1
+#     total = 0
+#     single_row = 0
+#     while single_row < len(inventory_list):
+#         print_row = sheet1.row(single_row + 5)
+#         for i in range(0, 4):
+#             print_row.write(i, inventory_list[single_row][i])
+#         total += inventory_list[single_row][3]
+#         single_row +=1
 
-    sheet1.write(1, 1, total)
-    sheet1.write(2, 1, total / 2)
-    book.save(XLS_FOLDER + file_name)
+#     sheet1.write(1, 1, total)
+#     sheet1.write(2, 1, total / 2)
+#     book.save(XLS_FOLDER + file_name)
 
-    flash("Successfully saved " + file_name)
-    return redirect(url_for('save_inv'))
+#     flash("Successfully saved " + file_name)
+#     return redirect(url_for('save_inv'))
 
-@app.route("/add_skus")
-def add_skus():
-    return render_template('add_skus.html')
+# @app.route("/add_skus")
+# def add_skus():
+#     return render_template('add_skus.html')
 
-@app.route("/add_skus", methods=['POST'])
-# ############### This needs help. Need to be able to specify which table the sku is being added to
-def add_new_skus():
-    if request.form['btn'] == 'Add to Inventory':
-        bar_code = request.form.get('i_sku')
-        value = int(request.form.get('i_value'))
-        amount = int(request.form.get('i_amount'))
-        # file_name = request.form.get('i_file_name')
-    else:
-        bar_code = request.form.get('s_sku')
-        value = int(request.form.get('s_value'))
-        amount = -1 * int(request.form.get('s_amount'))
-        # file_name = request.form.get('s_file_name')
-    file_type = "I"
-    model.check_inventory(file_type, bar_code, value, amount)
-    # model.add_to_table(file_type, file_name, bar_code, value, amount)
-    flash("Successfully added " + bar_code)
-    return redirect(url_for('add_skus'))
+# @app.route("/add_skus", methods=['POST'])
+# # ############### This needs help. Need to be able to specify which table the sku is being added to
+# def add_new_skus():
+#     if request.form['btn'] == 'Add to Inventory':
+#         bar_code = request.form.get('i_sku')
+#         value = int(request.form.get('i_value'))
+#         amount = int(request.form.get('i_amount'))
+#         # file_name = request.form.get('i_file_name')
+#     else:
+#         bar_code = request.form.get('s_sku')
+#         value = int(request.form.get('s_value'))
+#         amount = -1 * int(request.form.get('s_amount'))
+#         # file_name = request.form.get('s_file_name')
+#     file_type = "I"
+#     model.check_inventory(file_type, bar_code, value, amount)
+#     # model.add_to_table(file_type, file_name, bar_code, value, amount)
+#     flash("Successfully added " + bar_code)
+#     return redirect(url_for('add_skus'))
+
+
+@app.route("/clear")
+def clear():
+    session.clear()
+    return redirect(url_for("index"))
+
 
 
 if __name__ == "__main__":
